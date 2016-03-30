0c97048a008fa7e3b7384c2e0d8c6c97f6c77030
daminlocnguyen <daminlocnguyen@gmail.com>
2016-03-30 19:36:00 +0700

    Update .js

--- .js
+++ .js
@@ -117,7 +117,7 @@ $(window).load(function() {
 			$(divpopup).attr('id', 'myModal');
 			$(divpopup).attr('class', 'linhnguyen-modal');
 			$( "body" ).append(divpopup);
-			$("#myModal").html("<a href='http://chaua.com.vn/' target='_blank'><img src='http://2.pik.vn/2016b1e1e238-5c91-45a6-a3c9-c157637757c0.jpg' width='500px'/>Đào tạo seo chuyên sâu</a>. Học tại nhà Linh Nguyễn. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			$("#myModal").html("<a href='http://chaua.com.vn/' target='_blank'><img src='http://2.pik.vn/2016b1e1e238-5c91-45a6-a3c9-c157637757c0.jpg' width='500px'/>Dịch vụ SEO</a>. Chaua.com.vn. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			$('#myModal').linhnguyen($('#myModal').data());
 		}
 });
